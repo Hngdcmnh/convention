8007e9481834d970661292ca3a6228221f051bf0
Hoàng Minh <58630186+Hngdcmnh@users.noreply.github.com>
2022-07-05 22:37:47 +0700

    useage

--- lib/useage/collection.dart
+++ lib/useage/collection.dart
@@ -1,6 +1,51 @@
+import 'package:convention/useage/string.dart';
+
 /// - DO use collection literals when possible.
 /// - DON’T use .length to see if a collection is empty.
 /// - AVOID using Iterable.forEach() with a function literal.
 /// - DON’T use List.from() unless you intend to change the type of the result.
 /// - DO use whereType() to filter a collection by type.
 /// - AVOID using cast().
+
+void main() {
+  /// - DO use collection literals when possible.
+
+  var goodList = <int>[];
+  var goodMap = <String, String>{};
+  var goodSet = <int>{};
+
+  var badMap = Map<String, String>();
+  var badSet = Set<int>();
+
+  /// - DON’T use .length to see if a collection is empty.
+  if (goodList.isEmpty) {}
+  if (goodList.length == 0) {}
+
+  /// - AVOID using Iterable.forEach() with a function literal.
+
+  for (final a in goodList) {}
+
+  goodList.forEach((element) {
+    print(element);
+  });
+
+  /// - DON’T use List.from() unless you intend to change the type of the result.
+
+  var iterable = [1, 2, 3];
+
+  print(iterable.toList().runtimeType); // int
+  print(List.from(iterable).runtimeType); // dynamic
+  var ints = List<int>.from(iterable);
+
+  /// - DO use whereType() to filter a collection by type.
+
+  var objects = [1, 'a', 2, 'b', 3];
+  var badFilter = objects.where((e) => e is int);
+  var goodFilter = objects.whereType<int>();
+
+
+  /// - AVOID using cast().
+
+  
+
+}
--- lib/useage/function.dart
+++ lib/useage/function.dart
@@ -1,3 +1,39 @@
+import 'package:convention/documentation/comment.dart';
+
 /// - DO use a function declaration to bind a function to a name.
 /// - DON’T create a lambda when a tear-off will do.
 /// - DO use = to separate a named parameter from its default value.
+
+
+void main()
+{
+  /// - DO use a function declaration to bind a function to a name.
+
+  void goodFunction() {}
+  void badFunction = () {};
+
+  /// - DON’T create a lambda when a tear-off will do.
+
+  var tearOff = doSomeThing;
+  var lambda = () => doSomeThing;
+
+  /// - DO use = to separate a named parameter from its default value.
+
+  void goodParamFunction({int a =0})
+  {
+
+  }
+
+  void badParamFunction({int a:0})
+  {
+
+  }
+
+
+
+}
+
+int doSomeThing()
+{
+  return 2;
+}
